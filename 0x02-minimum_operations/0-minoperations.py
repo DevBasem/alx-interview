af1d24f057423c45c7d221dae62e7f106fd4cea1
DevBasem <engr.basemahmed@gmail.com>
2024-06-13 13:43:23 +0000

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python3
-"""
-Defines a method that calculates the fewest number of operations needed
-to result in exactly n H characters in a file.
-"""
-
-def minOperations(n):
+""" Minimum Operations
     """
-    Calculates the fewest number of operations needed to result in exactly
-    n H characters in a file.
 
-    Args:
-        n (int): The desired number of H characters.
 
-    Returns:
-        int: The fewest number of operations needed.
-    """
-    if n <= 1:
-        return n
-
-    # Function to calculate prime factors of a number
-    def prime_factors(num):
-        factors = []
-        while num % 2 == 0:
-            factors.append(2)
-            num //= 2
-        for i in range(3, int(num**0.5) + 1, 2):
-            while num % i == 0:
-                factors.append(i)
-                num //= i
-        if num > 2:
-            factors.append(num)
-        return factors
-
-    # Get prime factors of n
-    factors = prime_factors(n)
-
-    # The sum of prime factors will give the minimum operations needed
-    return sum(factors)
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
