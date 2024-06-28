e6cb455b8fe1347a93e5212471a0663aa86615ff
DevBasem <engr.basemahmed@gmail.com>
2024-06-27 18:58:27 -0700

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,31 +3,31 @@
 0-validate_utf8.py
 """
 
+def countLeadingSetBits(num):
+    """Returns the number of leading set bits (1) in the given number."""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
+
 
 def validUTF8(data):
-    # Function to check if a byte is valid as the start of a UTF-8 character
-    def is_start_of_utf8(byte):
-        return (byte >> 7) == 0b0 or (byte >> 6) == 0b10
-
-    i = 0
-    while i < len(data):
-        # Determine number of bytes in current UTF-8 sequence
-        if (data[i] >> 7) == 0b0:
-            num_bytes = 1
-        elif (data[i] >> 5) == 0b110:
-            num_bytes = 2
-        elif (data[i] >> 4) == 0b1110:
-            num_bytes = 3
-        elif (data[i] >> 3) == 0b11110:
-            num_bytes = 4
-        else:
-            return False
-
-        # Check that subsequent bytes (if any) start with 0b10
-        for j in range(1, num_bytes):
-            if i + j >= len(data) or not is_start_of_utf8(data[i + j]):
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+    remaining_bytes = 0
+    for i in range(len(data)):
+        if remaining_bytes == 0:
+            remaining_bytes = countLeadingSetBits(data[i])
+            # 1-byte (format: 0xxxxxxx)
+            if remaining_bytes == 0:
+                continue
+            # A character in UTF-8 can be 1 to 4 bytes long
+            if remaining_bytes == 1 or remaining_bytes > 4:
                 return False
-
-        i += num_bytes
-
-    return True
+        else:
+            # Checks if current byte has format 10xxxxxx
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                return False
+        remaining_bytes -= 1
+    return remaining_bytes == 0
