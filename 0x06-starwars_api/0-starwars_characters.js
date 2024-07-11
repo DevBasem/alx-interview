aa65607e5f1e6b66559e251a9118536ab46fda22
DevBasem <engr.basemahmed@gmail.com>
2024-07-11 16:57:53 -0700

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,26 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-});
+);
