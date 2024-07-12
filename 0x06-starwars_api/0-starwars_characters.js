b3a61ed52c34870cb8d33eb4be9ecb6cb4179d69
DevBasem <engr.basemahmed@gmail.com>
2024-07-11 17:00:27 -0700

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
 
+// Imports the request module for making HTTP requests
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
+// Recursively requests and prints character names from an array of URLs
+const fetchCharacterNames = (characters, index) => {
+  if (index === characters.length) return;
+  request(characters[index], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    fetchCharacterNames(characters, index + 1);
   });
 };
 
+// Fetches character URLs from the Star Wars API for a given movie ID
 request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  (error, response, body) => {
+    if (error) throw error;
+    const characters = JSON.parse(body).characters;
+    fetchCharacterNames(characters, 0);
   }
 );
+
