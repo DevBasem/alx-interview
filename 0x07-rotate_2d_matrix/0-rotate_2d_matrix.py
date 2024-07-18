901057050dc3b8c7312684a12a63acb865e2d00a
DevBasem <engr.basemahmed@gmail.com>
2024-07-18 01:08:30 -0700

    task 0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module to rotate a 2D matrix 90 degrees clockwise.
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2D matrix 90 degrees clockwise in-place.
+
+    Args:
+        matrix (list of list of int): The 2D matrix to rotate.
+
+    Returns:
+        None
+    """
+    n = len(matrix)
+    for layer in range(n // 2):
+        first = layer
+        last = n - layer - 1
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
+
+# Ensure the script is executable
+if __name__ == "__main__":
+    import os
+    os.chmod(__file__, 0o755)
